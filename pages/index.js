e70ec72f10b768ed213410c64c020d0b53d0801f
jaimepeijl <j.d.peijl@gmail.com>
2022-12-06 18:08:31 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,60 @@
-import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [input, setInput] = useState("");
+  const [title, setTitle] = useState("")
   const [result, setResult] = useState();
+  const [loading, toggleLoading] = useState(false)
+  const [div, toggleDiv] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
+    toggleLoading(true)
+    toggleDiv(true)
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ title: title, input: input }),
     });
+    toggleLoading(false)
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    console.log(result)
+    console.log(response)
   }
 
   return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
-
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
         <form onSubmit={onSubmit}>
+          <div className={styles.title}>
+          <h3>Jaime's</h3>
+          <h3>Verhalen verteller</h3>
+        </div>
           <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+              className={styles.input}
+              type="text"
+              name="title"
+              placeholder="Verzin eerst zelf een titel"
+              value={title}
+              onChange={(e)=>setTitle(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <textarea
+            className={styles.textarea}
+            name="input"
+            placeholder="Het moet gaan over ... "
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <input type="submit" value="Genereer een Verhaal" />
         </form>
-        <div className={styles.result}>{result}</div>
+            <div className={styles.result}>
+              {loading && <span>Aan het laden ...</span>}
+              <h2>{title}</h2>
+              {result}
+            </div>
       </main>
-    </div>
   );
 }
